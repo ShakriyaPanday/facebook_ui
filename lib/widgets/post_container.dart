1d3e3b55ef8834ab306408f3362a293e05cc6b77
Shakriya Panday <fenom.shakriyapanday@gmail.com>
2022-11-21 22:05:39 +0545

    Uer and time info added in post

--- lib/widgets/post_container.dart
+++ lib/widgets/post_container.dart
@@ -1,4 +1,5 @@
 import 'package:facebook/models/post_model.dart';
+import 'package:facebook/widgets/profileavator.dart';
 import 'package:flutter/material.dart';
 
 class PostContainer extends StatelessWidget {
@@ -12,9 +13,12 @@ class PostContainer extends StatelessWidget {
       color: Colors.white,
       margin: const EdgeInsets.symmetric(vertical: 5.0),
       padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [_PostHeader(post: post)],
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [_PostHeader(post: post)],
+        ),
       ),
     );
   }
@@ -27,6 +31,44 @@ class _PostHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: [
+        ProfileAvatar(imageUrl: post.user.imageUrl),
+        SizedBox(
+          width: 10,
+        ),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                post.user.name,
+                style: const TextStyle(fontWeight: FontWeight.w600),
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    "${post.timeAgo} • ",
+                    style: TextStyle(color: Colors.grey[600], fontSize: 12.0),
+                  ),
+                  Icon(
+                    Icons.public,
+                    size: 12.0,
+                    color: Colors.grey[600],
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+        TextButton(
+            onPressed: (() => print("more")),
+            child: const Icon(
+              Icons.more_horiz,
+              color: Colors.black,
+            ))
+      ],
+    );
   }
 }
