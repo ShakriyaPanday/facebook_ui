18c9425b5830d103d4e1311b22f6ee27c8302ce1
Shakriya Panday <fenom.shakriyapanday@gmail.com>
2022-11-21 18:39:05 +0545

    Screen added

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,58 @@
+import 'package:facebook/config/palette.dart';
+import 'package:facebook/data/data.dart';
+import 'package:facebook/widgets/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(slivers: [
+        SliverAppBar(
+          floating: true,
+          systemOverlayStyle: SystemUiOverlayStyle.light,
+          backgroundColor: Colors.white,
+          centerTitle: false,
+          title: Text(
+            "facebook",
+            style: TextStyle(
+              fontSize: 28.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: -1.2,
+              color: Palette.facebookBlue,
+            ),
+          ),
+          actions: [
+            CircleButton(
+              icon: Icons.search,
+              iconsize: 28.0,
+              onpressed: (() => print("Search pressed")),
+            ),
+            CircleButton(
+              icon: MdiIcons.facebookMessenger,
+              iconsize: 28.0,
+              onpressed: (() => print("Messenger pressed")),
+            ),
+          ],
+        ),
+        SliverToBoxAdapter(
+            child: CreatePostContainer(
+          currentUser: currentUser,
+        )),
+        SliverPadding(
+          padding: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 5.0),
+          sliver: SliverToBoxAdapter(
+            child: Rooms(onlineUser: onlineUsers),
+          ),
+        ),
+        SliverFillRemaining(
+          child: Column(
+            children: [Text('some cool text here')],
+          ),
+        )
+      ]),
+    );
+  }
+}
---
+++ lib/screen/screen.dart
@@ -0,0 +1,1 @@
+export 'package:facebook/screen/home_screen.dart';
