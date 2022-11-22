9cad9e57fa6cea82e0002bdf1b78841ebcbbe1c2
Shakriya Panday <fenom.shakriyapanday@gmail.com>
2022-11-22 22:22:26 +0545

    list of screens and icons added

--- lib/screen/nav_screen.dart
+++ lib/screen/nav_screen.dart
@@ -1,1 +1,32 @@
+import 'package:facebook/screen/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+
+class NavScreen extends StatefulWidget {
+  const NavScreen({super.key});
+
+  @override
+  State<NavScreen> createState() => _NavScreenState();
+}
+
+class _NavScreenState extends State<NavScreen> {
+  final List<Widget> _screens = [
+    HomeScreen(),
+    Scaffold(),
+    Scaffold(),
+    Scaffold(),
+    Scaffold(),
+  ];
+  final List<IconData> _icons = const [
+    Icons.home,
+    Icons.ondemand_video,
+    MdiIcons.accountCircleOutline,
+    MdiIcons.accountGroupOutline,
+    MdiIcons.bellOutline,
+    Icons.menu
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/screen/screen.dart
+++ lib/screen/screen.dart
@@ -1,1 +1,2 @@
 export 'package:facebook/screen/home_screen.dart';
+export 'package:facebook/screen/nav_screen.dart';
